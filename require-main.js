1d265a1427d286da372e7ef183c4c40aa9cbe991
Appleskiller <appleskiller@163.com>
2016-01-03 11:04:02 +0000

    -

--- require-main.js
+++ require-main.js
@@ -11,42 +11,44 @@ requirejs([
     $(document).ready(function () {
         
         function setOptions(opts) {
-            (new Function('o = ' + opts))();
-            $('#svg').highcharts(o);
-            (new Function('o = ' + opts))();
-            o.chart = o.chart || {};
-            o.chart.renderer = "CanvasRenderer"
-            $('#canvas').highcharts(o);
+            var options = null;
+            options = (new Function(opts))();
+            $('#svg').highcharts(options);
+            options = (new Function(opts))();
+            options.chart = options.chart || {};
+            options.chart.renderer = "CanvasRenderer"
+            $('#canvas').highcharts(options);
         }
         
-        var options = {
-            xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            },
-            yAxis: {
-                title: {
-                    text: 'Temperature (°C)'
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 2,
-                    color: '#808080'
-                }]
-            },
-            series: [{
-                name: 'Tokyo',
-                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-            }]
-        }
+        var settingText = "return {\r\n" + 
+        "    xAxis: {\r\n" + 
+        "        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']\r\n" + 
+        "    },\r\n" + 
+        "    yAxis: {\r\n" + 
+        "        title: {\r\n" + 
+        "            text: 'Temperature (°C)'\r\n" + 
+        "        },\r\n" + 
+        "        plotLines: [{\r\n" + 
+        "            value: 0,\r\n" + 
+        "            width: 2,\r\n" + 
+        "            color: '#808080'\r\n" + 
+        "        }]\r\n" + 
+        "    },\r\n" + 
+        "    series: [{\r\n" + 
+        "        name: 'Tokyo',\r\n" + 
+        "        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]\r\n" + 
+        "    }]\r\n" + 
+        "}"
         
-        setOptions(JSON.stringify(options));
+        setOptions(settingText);
         
         var editor = ace.edit("editor");
         editor.setOptions({
             fontSize: 18,
-            mode: "ace/mode/text" ,
+            mode: "ace/mode/javascript" ,
+            theme: "ace/theme/tomorrow_night_eighties",
         })
+        editor.setValue(settingText);
         
         $('#btn').click(function () {
             try {
